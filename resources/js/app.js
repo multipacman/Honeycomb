1372919a2d0da79f5dc9b668b6c5636ade9da93b
multipacman <abdulhadhee1@gmail.com>
2021-12-28 22:16:18 +0530

    added graphql mutation and removed components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,11 @@
 import "./bootstrap";
 import Vue from "vue";
 import Board from "./Board";
-import Card from "./components/Card";
-import CardAddButton from "./components/CardAddButton";
-import List from "./components/List";
 import apolloProvider from "./apollo.config";
 
 window.Vue = Vue;
 
 Vue.component("Board", Board);
-Vue.component("Card", Card);
-Vue.component("CardAddButton", CardAddButton);
-Vue.component("List", List);
 
 const app = new Vue({
     el: "#app",
