574dbdf60065708552923b325fec4c0f366afafc
multipacman <abdulhadhee1@gmail.com>
2021-12-28 22:17:09 +0530

    created new tailwind utility to use for other components

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="card bg-white rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2"
+        class="shadow-card bg-white rounded-sm p-2 cursor-pointer text-sm hover:bg-gray-100 mb-2"
     >
         {{ card.title }}
     </div>
@@ -13,9 +13,3 @@ export default {
     }
 };
 </script>
-
-<style scoped>
-.card {
-    box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
     purge: [],
     darkMode: "media", // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {
+            boxShadow: {
+                card: "0 1px 0 rgba(9, 30, 66, 0.25)"
+            }
+        }
     },
     variants: {
         extend: {}
